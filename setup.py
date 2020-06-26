69de620c651d1306ffd409a885ada045a4ef46e0
Dr. Torge Szczepanek <info@cygnusnetworks.de>
2020-06-26 12:54:32 +0200

    Version bump and update authors

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@
 from setuptools import setup
 
 setup(name='check_ups_apc.py',
-	version='0.12',
+	version='0.13',
 	description='Nagios Check for APC USVs',
-	author='Lukas Schauer',
-	author_email='l.schauer@cygnusnetworks.de',
+	author='Lukas Schauer, Dr. Torge Szczepanek',
+	author_email='debian@cygnusnetworks.de',
 	license='Apache 2.0',
 	packages=['ups_apc_snmp'],
 	scripts=['check_ups_apc'],
