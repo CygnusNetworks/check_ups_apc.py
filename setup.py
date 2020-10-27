082025319fb6cd686cb9fd3e4532c0094772ee5c
Dr. Torge Szczepanek <info@cygnusnetworks.de>
2020-10-27 12:39:49 +0100

    Version bump and entry_point

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@
 from setuptools import setup
 
 setup(name='check_ups_apc.py',
-	version='0.13',
+	version='0.14',
 	description='Nagios Check for APC USVs',
 	author='Lukas Schauer, Dr. Torge Szczepanek',
 	author_email='debian@cygnusnetworks.de',
 	license='Apache 2.0',
 	packages=['ups_apc_snmp'],
-	scripts=['check_ups_apc'],
+	entry_points={'console_scripts': ["check_ups_apc = ups_apc_snmp.nagios_plugin:main"]},
 	zip_safe=False,
 	install_requires=['configparser', 'nagiosplugin', 'pysnmp'])
